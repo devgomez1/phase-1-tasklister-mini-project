b83cb73e02806cd5e625d7d99fd357f36f5c8337
Devin Elliot Gomez <devinelliotgomez@gmail.com>
2023-11-14 13:10:21 -0600

    finished challenge

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+let form = document.getElementById("create-task-form");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let value = e.target["new-task-description"].value;
+  let todos = document.createElement("li");
+  let list = document.getElementById("tasks");
+  todos.textContent = value;
+  list.append(todos);
 });
